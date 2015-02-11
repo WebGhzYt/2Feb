6e7915d7111c1717ebe00895336aa9cb8039c0de
Rohit <engrohitjain5@gmail.com>
2015-02-11 11:36:26 -0800

    Work OF 11/2/2015

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 List.create!(name: "Input")
 List.create!(name: "keybord",pid: 1)
+List.create!(name: "Optical",pid: 2)
+List.create!(name: "Mechnicl",pid: 2)
 List.create!(name: "output")
 List.create!(name: "Printer",pid: 3)
+List.create!(name: "Monitor",pid: 3)
+List.create!(name: "LCD",pid: 7)
+List.create!(name: "LED",pid: 7)
+List.create!(name: "Scanner",pid: 3)
 
 
